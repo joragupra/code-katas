38854e8eaba6eb04da734c54dac9186775fa891a
Jorge Agudo <joragupra@gmail.com>
2013-06-21 15:50:29 +0200

    First test

---
+++ src/main/java/com/joragupra/codekatas/Game.java
@@ -0,0 +1,15 @@
+package com.joragupra.codekatas;
+
+/**
+ * Hello world!
+ */
+public class Game {
+
+	public void roll(int pins) {
+
+	}
+
+	public int score() {
+		return 0;
+	}
+}
---
+++ src/test/java/com/joragupra/codekatas/BowlingGameTest.java
@@ -0,0 +1,14 @@
+package com.joragupra.codekatas;
+
+import junit.framework.TestCase;
+
+public class BowlingGameTest extends TestCase {
+
+	public void testGutterGame() {
+		Game g = new Game();
+		for (int i = 0; i < 20; i++) {
+			g.roll(0);
+		}
+		assertEquals(0, g.score());
+	}
+}
