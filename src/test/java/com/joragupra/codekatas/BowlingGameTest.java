19a00861c208404ef6827d02b4ac1a458d515ff1
Jorge Agudo <joragupra@gmail.com>
2013-06-21 15:56:20 +0200

    Second test

--- src/test/java/com/joragupra/codekatas/BowlingGameTest.java
+++ src/test/java/com/joragupra/codekatas/BowlingGameTest.java
@@ -4,11 +4,25 @@ import junit.framework.TestCase;
 
 public class BowlingGameTest extends TestCase {
 
-	public void testGutterGame() {
-		Game g = new Game();
-		for (int i = 0; i < 20; i++) {
-			g.roll(0);
+	private Game g;
+
+	protected void setUp() {
+		g = new Game();
+	}
+
+	private void rollMany(int n, int pins) {
+		for (int i = 0; i < n; i++) {
+			g.roll(pins);
 		}
+	}
+
+	public void testGutterGame() {
+		rollMany(20, 0);
 		assertEquals(0, g.score());
+	}
+
+	public void testAllOnes() {
+		rollMany(20, 1);
+		assertEquals(20, g.score());
 	}
 }
